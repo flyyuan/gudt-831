c14ae33b2fcc3b8819c4610b38906690429cf934
flyyuan <740004544@qq.com>
2019-11-06 22:48:00 +0800

    使用CLion

--- test.c
+++ test.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
-main(){
-    putchar(7/8+'0');
+#include <math.h>
+int main(){
+    int x = 3;
+    int r =sqrt(x),i;
+    printf("%d",r);
+    for(i=2;i<=sqrt(x);i++){
+        printf("%d,",i);
+        printf("!%d!",r);
+        fabs(1);
+    }
 }
